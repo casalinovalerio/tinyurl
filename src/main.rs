0db6b8a72f1183a54a2bfcb23630a15988cc9f19
Valerio Casalino <casalinovalerio.cv@gmail.com>
2020-01-26 22:48:44 +0100

    No custom option for api endpoint
    Last implementation I think... Really nothing more to do

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,12 @@ extern crate reqwest;
 extern crate clap;
 
 use clap::{Arg, App};
-use std::process::exit;
 
 static APP_NAME: &'static str = "TinyUrl 🔗 shortener CLI";
 static VERSION: &'static str = "1.1";
 static AUTHOR: &'static str = "Valerio Casalino <casalinovalerio.cv@gmail.com>";
 static DESCRIPTION: &'static str = "CLI Rust wrapper for tinyurl's API";
+static ENDPOINT: &'static str = "https://tinyurl.com/api-create.php";
 
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
@@ -21,23 +21,12 @@ async fn main() -> Result<(), reqwest::Error> {
                 .help("url 🔗 to shorten ✂")
                 .required(true)
                 .multiple(false))
-        .arg(
-            Arg::with_name("custom")
-                .short("c")
-                .long("custom")
-                .multiple(false)
-                .required(false)
-                .takes_value(true)
-                .help("Custom alias for your link 😎")
-        )
         .get_matches();
 
     let to_shorten = matches.value_of("input").unwrap();
-    let url = format!("https://tinyurl.com/api-create.php?url={}", to_shorten);
+    let url = format!("{}?url={}", ENDPOINT, to_shorten);
     let res = reqwest::get(url.as_str()).await?.text().await?;
 
     println!("{}", res);
-
-
     Ok(())
 }
